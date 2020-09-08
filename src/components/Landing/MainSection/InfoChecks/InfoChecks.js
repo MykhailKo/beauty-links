44fc964681347aac0eeb2a863f91a1279709eae9
metamodern <shabanovbunya@gmail.com>
2020-09-08 16:25:12 +0300

    fixed iteration in InfoChecks

--- src/components/Landing/MainSection/InfoChecks/InfoChecks.js
+++ src/components/Landing/MainSection/InfoChecks/InfoChecks.js
@@ -1,37 +1,47 @@
-import React from 'react';
+import React from "react";
 
-import SecTitle from '../SecTitle/SecTitle';
+import SecTitle from "../SecTitle/SecTitle";
 
-import styles from './InfoChecks.module.css';
+import styles from "./InfoChecks.module.css";
 
-const InfoChecks = ({ secTitle, firstTitle, secondTitle, firstList, secondList }) => {
-  return(
-    <div className={'container'}>
-    <section className={styles.checksSec}>
-      <SecTitle title={secTitle} />
-      <div className={styles.checksBlockTop}>
-        <h3 className={styles.checksTitle}>{firstTitle}</h3>
-        <ul className={styles.checksList}>
-          {
-            firstList.map((item, key) => {
-              return <li className={styles.checksLi}>{item.text}</li>
-            })
-          }
-        </ul>
-      </div>
-      <div className={styles.checksBlockBottom}>
-        <h3 className={styles.checksTitle}>{secondTitle}</h3>
-        <ul className={styles.checksList}>
-          {
-            secondList.map((item, key) => {
-              return <li className={styles.checksLi}>{item.text}</li>
-            })
-          }
-        </ul>
-      </div>
-    </section>
+const InfoChecks = ({
+  secTitle,
+  firstTitle,
+  secondTitle,
+  firstList,
+  secondList,
+}) => {
+  return (
+    <div className={"container"}>
+      <section className={styles.checksSec}>
+        <SecTitle title={secTitle} />
+        <div className={styles.checksBlockTop}>
+          <h3 className={styles.checksTitle}>{firstTitle}</h3>
+          <ul className={styles.checksList}>
+            {firstList.map((item, index) => {
+              return (
+                <li className={styles.checksLi} key={index}>
+                  {item.text}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className={styles.checksBlockBottom}>
+          <h3 className={styles.checksTitle}>{secondTitle}</h3>
+          <ul className={styles.checksList}>
+            {secondList.map((item, index) => {
+              return (
+                <li className={styles.checksLi} key={index}>
+                  {item.text}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
 export default InfoChecks;
