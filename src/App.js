f2c7abbc5fb4a8e9564be911b95deb418734c95b
metamodern <shabanovbunya@gmail.com>
2020-09-09 15:26:55 +0300

    add footer from another folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Header from "./components/Landing/Header/Header";
-import Footer from "./components/Landing/Footer/Footer";
+import Footer from "./components/landing/Footer/Footer";
 import MainSection from "./components/Landing/MainSection/MainSection";
 
 function App() {
