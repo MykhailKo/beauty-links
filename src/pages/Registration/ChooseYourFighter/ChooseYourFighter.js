9c7635aa36d650359f315fb37d5326ee07bcccc4
MykhailKo <tuareg1812@gmail.com>
2020-09-10 21:02:19 +0300

    ChooseFighter fixes

--- src/pages/Registration/ChooseYourFighter/ChooseYourFighter.js
+++ src/pages/Registration/ChooseYourFighter/ChooseYourFighter.js
@@ -1,19 +1,19 @@
 import React from "react";
 
 import ShBox from "../../../components/ShBox/ShBox";
-import SecTitle from "../../Landing/SecTitle/SecTitle";
+import RegTitle from "../RegTitle/RegTitle";
 
 import styles from "./ChooseYourFighter.module.scss";
 import SideSelect from "../../../components/SideSelect/SideSelect";
 
 const ChooseYourFighter = () => {
   return (
-    <ShBox>
-      <div className={styles.ChooseFiglter}>
-        <SecTitle title={"Почти готово!"} />
-        <div className={styles.SubTitle}>
-          Не забудьте подтвердить свой e-mail.
-        </div>
+    <ShBox padding={'2em 0 4em 0'}>
+      <RegTitle text={"Почти готово!"} />
+      <div className={styles.SubTitle}>
+        Не забудьте подтвердить свой e-mail.
+      </div>
+      <div className={styles.ChooseFighter}>
         <div className={styles.SelectorBlock}>
           <SideSelect
             imagePath={"/assets/img/master.png"}
