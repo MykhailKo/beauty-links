8386e6d07d21d4a0279f83cf800a2f5afcb118c7
MykhailKo <tuareg1812@gmail.com>
2020-09-11 13:53:26 +0300

    RegistrationFlow first step

---
+++ src/pages/Registration/RegistrationFlow/RegPersData/RegPersData.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import RegTitle from "../../RegTitle/RegTitle";
+import RegSubTitle from "../../RegSubTitle/RegSubTitle";
+
+import styles from "./RegPersData.module.scss";
+
+const RegPersData = () => {
+  return (
+    <div>
+      <RegTitle text={"Укажите свои личные данные"} />
+      <RegSubTitle
+        text={"Эти данные будут отображены в вашем профиле мастера."}
+      />
+    </div>
+  );
+};
+
+export default RegPersData;
--- src/pages/Registration/RegistrationFlow/RegistrationFlow.js
+++ src/pages/Registration/RegistrationFlow/RegistrationFlow.js
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 
 import Button from "../../../components/Button/Button";
 import ShBox from "../../../components/ShBox/ShBox";
+import Stepper from "../../../components/Stepper/Stepper";
+import RegPersData from "./RegPersData/RegPersData";
 
 import styles from "./RegistrationFlow.module.scss";
-import Stepper from "../../../components/Stepper/Stepper";
 
 const RegistrationFlow = ({ nextStep }) => {
   const [step, setStep] = useState(1);
@@ -12,7 +13,9 @@ const RegistrationFlow = ({ nextStep }) => {
   return (
     <ShBox padding={"3em 3em"}>
       <Stepper step={step} />
-      <div className={styles.stepperContent}></div>
+      <div className={styles.stepperContent}>
+        {step === 1 && <RegPersData />}
+      </div>
       <Button
         text={"Продолжить"}
         onClick={() => {
