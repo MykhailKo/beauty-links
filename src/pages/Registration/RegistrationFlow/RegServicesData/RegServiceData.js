0962c36b05d350de44db8aeadd54864468d28941
MykhailKo <tuareg1812@gmail.com>
2020-09-13 13:52:44 +0300

    RegFlow carousel

--- src/pages/Registration/RegistrationFlow/RegServicesData/RegServiceData.js
+++ src/pages/Registration/RegistrationFlow/RegServicesData/RegServiceData.js
@@ -3,8 +3,19 @@ import React from "react";
 import Button from "../../../../components/Button/Button";
 import RegTitle from "../../RegTitle/RegTitle";
 import RegSubTitle from "../../RegSubTitle/RegSubTitle";
+import ServiceCarousel from "../../../../components/ServiceCarousel/ServiceCarousel";
 
 import styles from "./RegServiceData.module.scss";
+
+const serviceCats = [
+  { name: "Косметология", id: "cosm" },
+  { name: "Маникюр/педикюр", id: "nails" },
+  { name: "Массаж и SPA", id: "spa" },
+  { name: "Уход за волосами", id: "hair" },
+  { name: "Стоматология", id: "stomat" },
+  { name: "Эпиляция", id: "epil" },
+  { name: "Макияж", id: "makeup" },
+];
 
 const RegServiceData = ({ nextStep }) => {
   return (
@@ -15,7 +26,7 @@ const RegServiceData = ({ nextStep }) => {
           "Для начала, добавьте свои основные услуги и цены. Не переживайте, вы сможете их изменить в любое время в своём личном кабинете."
         }
       />
-
+      <ServiceCarousel serviceCats={serviceCats} />
       <Button text={"Продолжить"} onClick={nextStep} />
     </div>
   );
