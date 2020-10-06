1932eb554d62990504d544002dbaf329eb374976
metamodern <shabanovbunya@gmail.com>
2020-10-06 20:12:40 +0300

    add gmaps key

--- src/pages/MasterPage/MasterContacts/MasterContacts.js
+++ src/pages/MasterPage/MasterContacts/MasterContacts.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useHttp } from "../../../hooks/useHttp";
 import MasterPage from "../MasterPage";
 
@@ -10,26 +10,42 @@ const MasterContacts = ({
   socnet = {
     pint: "pinterest.com",
     fb: "facebook.com",
-    inst: "instagrsam.com",
+    inst: "instagram.com",
   },
 }) => {
   const [numberShown, setNumberShown] = useState(false);
-
+  const [addressByGoogle, setAddressByGoogle] = useState(
+    "Адрес не предоставлен"
+  );
+  const fetchLocation = useCallback(async () => {
+    try {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${address.lat},${address.lon}&key=${process.env.REACT_APP_API_GMAPS}&language=ru&region=RU`
+      );
+      const data = await response.json();
+      console.log(data.results[0].formatted_address);
+      setAddressByGoogle(data.results[0].formatted_address);
+    } catch (error) {}
+  }, [address.lat, address.lon]);
+  useEffect(() => {
+    fetchLocation();
+    console.log(process.env.REACT_APP_API_GMAPS);
+  }, [fetchLocation]);
   return (
     <div className={styles.contactsWrap}>
       <iframe
         title={"map"}
         className={styles.map}
         id={"gmap_canvas"}
-        src={`https://maps.google.com/maps?q=${address.lat},${address.lon}&output=embed`}
+        src={`https://maps.google.com/maps?q=${address.lat},${address.lon}&key=${process.env.REACT_APP_API_GMAPS}&language=ru&region=UK&output=embed`}
         frameBorder={"0"}
         scrolling={"no"}
         marginHeight={"0"}
         marginWidth={"0"}
       ></iframe>
       <div className={styles.address}>
-        <span></span>
-        Украина, г. Харьков, ул. Сумская 35а
+        <span />
+        {addressByGoogle}
       </div>
       <div className={styles.phoneBlock}>
         <div
