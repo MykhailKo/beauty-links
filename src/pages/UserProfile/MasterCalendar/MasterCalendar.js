3c3291910afff128721571b8233c64b094b3a3e2
MykhailKo <tuareg1812@gmail.com>
2020-10-14 16:34:53 +0300

    calendar statuses, myprofile fetches

--- src/pages/UserProfile/MasterCalendar/MasterCalendar.js
+++ src/pages/UserProfile/MasterCalendar/MasterCalendar.js
@@ -1,65 +1,17 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useState, useCallback, useEffect, useContext } from "react";
 
 import ProfileTitle from "../../../components/ProfileTitle/ProfileTitle";
 import CalendarItem from "../../../components/CalendarItem/CalendarItem";
+import authContext from "../../../context/auth.context";
 
 import { useHttp } from "../../../hooks/useHttp";
 import styles from "./MasterCalendar.module.scss";
 
-const months = [
-  "Январь",
-  "Февраль",
-  "Март",
-  "Апрель",
-  "Май",
-  "Июнь",
-  "Июль",
-  "Август",
-  "Сентябрь",
-  "Октябрь",
-  "Ноябрь",
-  "Декабрь",
-];
-const getMonthName = () => {
-  let now = new Date();
-  return months[now.getMonth()];
-};
-
-const getFirstDayOfMonth = () => {
-  let now = new Date();
-  let date = new Date(now.getFullYear(), now.getMonth(), 1);
-  return date.getDay() - 1;
-};
-
-const getCalendar = (bookings) => {
-  const now = new Date();
-  const firstDay = getFirstDayOfMonth();
-  const lastDay =
-    (now.getMonth() === 1 ? 27 : (now.getMonth() + 1) % 2 === 0 ? 30 : 29) +
-    firstDay;
-  let calendar = [];
-  for (let i = 0; i < 35; i++) {
-    if (i < firstDay || i > lastDay) calendar.push({});
-    else {
-      let dayBookings = bookings.filter(
-        (booking) =>
-          booking.date ===
-          `${now.getFullYear()}-${now.getMonth() + 1}-${i - firstDay + 1}`
-      );
-      calendar.push({
-        day: i - firstDay + 1,
-        bookings: dayBookings,
-        status: 0,
-      });
-    }
-  }
-  return calendar;
-};
-
-const MasterCalendar = ({ masterId }) => {
+const MasterCalendar = () => {
   // const [bookings, setBookings] = useState([]);
+  // const [schedule, setSchedule] = useSatte({});
   // const {request, loading} = useHttp();
-
+  // const {id} = useContext(authContext);
   // const fetchCalendar = useCallback(async () => {
   //   try {
   //     const response = await request(
@@ -74,10 +26,23 @@ const MasterCalendar = ({ masterId }) => {
   //   }catch(error) {
   //     console.log(error)
   //   }
-  // }, [masterId, request])
-  // useEffect(() => {
-  //   fetchCalendar();
-  // }, [fetchCalendar]);
+  // }, [id, request])
+
+  // const fetchSchedule = useCallback(async () => {
+  //   try {
+  //     const response = await request(
+  //       "/api/v1.0/",
+  //       "GET",
+  //       null,
+  //       {}
+  //     );
+  //     if(response.status === 200) {
+  //       setSchedule(response);
+  //     }
+  //   }catch(error) {
+  //     console.log(error)
+  //   }
+  // }, [id, request])
 
   const bookings = [
     {
@@ -159,6 +124,110 @@ const MasterCalendar = ({ masterId }) => {
     },
   ];
 
+  const schedule = {
+    friday: {
+      available: {
+        to: "17:00",
+        from: "09:00",
+      },
+    },
+    monday: {
+      available: {
+        to: "20:00",
+        from: "08:00",
+      },
+    },
+    sunday: [],
+    tuesday: {
+      available: {
+        to: "20:00",
+        from: "08:00",
+      },
+    },
+    saturday: [],
+    thursday: {
+      available: {
+        to: "20:00",
+        from: "08:00",
+      },
+    },
+    wednesday: {
+      available: {
+        to: "20:00",
+        from: "08:00",
+      },
+    },
+  };
+
+  const months = [
+    "Январь",
+    "Февраль",
+    "Март",
+    "Апрель",
+    "Май",
+    "Июнь",
+    "Июль",
+    "Август",
+    "Сентябрь",
+    "Октябрь",
+    "Ноябрь",
+    "Декабрь",
+  ];
+  const days = [
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+  ];
+  const now = new Date();
+  const getMonthName = () => {
+    return months[now.getMonth()];
+  };
+
+  const getFirstDayOfMonth = () => {
+    let date = new Date(now.getFullYear(), now.getMonth(), 1);
+    return date.getDay() - 1;
+  };
+
+  const getDayStatus = (day) => {
+    let dayDate = new Date(day);
+    let weekAhead = new Date();
+    weekAhead.setDate(now.getDate() + 6);
+    if (schedule[days[dayDate.getDay()]].length === 0) return 3;
+    if (dayDate <= weekAhead && dayDate >= now) return 1;
+    //if (callToEndpoint(dayDate).length === 0) return 2;
+    return 0;
+  };
+
+  const getCalendar = () => {
+    const firstDay = getFirstDayOfMonth();
+    const lastDay =
+      (now.getMonth() === 1 ? 27 : (now.getMonth() + 1) % 2 === 0 ? 30 : 29) +
+      firstDay;
+    let calendar = [];
+    for (let i = 0; i < 35; i++) {
+      if (i < firstDay || i > lastDay) calendar.push({});
+      else {
+        let dayBookings = bookings.filter(
+          (booking) =>
+            booking.date ===
+            `${now.getFullYear()}-${now.getMonth() + 1}-${i - firstDay + 1}`
+        );
+        calendar.push({
+          day: i - firstDay + 1,
+          bookings: dayBookings,
+          status: getDayStatus(
+            `${now.getFullYear()}-${now.getMonth() + 1}-${i - firstDay + 1}`
+          ),
+        });
+      }
+    }
+    return calendar;
+  };
+
   return (
     <div className={styles.calendarWrap}>
       <ProfileTitle
@@ -168,6 +237,11 @@ const MasterCalendar = ({ masterId }) => {
         }
       />
       <div className={styles.monthTitle}>{getMonthName()}</div>
+      <ul className={styles.calendarLegend}>
+        <li>Выходной</li>
+        <li>Запись закрыта</li>
+        <li>Запись открыта</li>
+      </ul>
       <div className={styles.calendarBorder}>
         <ul className={styles.calendar}>
           <span>понедельник</span>
@@ -177,7 +251,7 @@ const MasterCalendar = ({ masterId }) => {
           <span>пятница</span>
           <span>суббота</span>
           <span>воскресенье</span>
-          {getCalendar(bookings).map((day, key) => {
+          {getCalendar().map((day, key) => {
             return <CalendarItem key={key} day={day} />;
           })}
           <span className={styles.calendarBottom}></span>
